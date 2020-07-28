f951a41f54d70f9a402115835be886233943ecfc
Daniel Appel <dan.s.appel@gmail.com>
2020-07-28 14:42:57 -0400

    Change date fields to native dates

--- holding-schema.rb
+++ holding-schema.rb
@@ -3,14 +3,33 @@ require 'active_record'
 def init_db
   ActiveRecord::Schema.define do
     create_table :records do |t|
-      t.string :bib_id
-      t.string :media_type
-      t.string :label_type
-      t.string :record_type
-      t.string :holding_string
-      t.boolean :is_index
-      t.boolean :incomplete
-      t.boolean :negation
+      t.integer :bib_id, array: true, default: []
+      t.string :bib_id_links, array: true, default: []
+      t.integer :item_ids, array: true, default: []
+      t.string :item_id_links, array: true, default: []
+      t.booleandaterit_location
+      t.string :allocation_rule
+      t.integer :accounting_unit
+      t.string :label_code
+      t.string :serial_code_1
+      t.string :serial_code_2
+      t.string :serial_code_3
+      t.string :serial_code_4
+      t.string :claim_on_date
+      t.string :receiving_location_code
+      t.string :vendor_code
+      t.string :update_count
+      t.integer :piece_count
+      t.string :e_check_in_code
+      t.string :media_type_code
+      t.date :updated_date
+      t.date :created_date
+      t.date :deleted_date
+      t.boolean :deleted
+      t.boolean :suppressed
+      t.jsonb :fixed_fields, default: {}
+      t.jsonb :var_fields, default: {}
+      t.jsonb :holdings, default: {}
     end
   end
 end
