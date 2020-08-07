dcb61141c244a6aca7b15950641a65e18de2aee6
Daniel Appel <dan.s.appel@gmail.com>
2020-08-07 14:21:44 -0400

    Add some more error handling and logging

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ def init
   )
   $logger = NYPLRubyUtil::NyplLogFormatter.new(STDOUT, level: ENV['LOG_LEVEL'])
   $db_fields = Hash.new { |h,k| h[k] = k.gsub(/([a-z])([^a-z])/){"#{$1}_#{$2.downcase}"}  }
+  $kinesis_client = NYPLRubyUtil::KinesisClient.new(
+    schema_string: ENV['SCHEMA_STRING'],
+    stream_name: ENV['STREAM_NAME']
+  )
 end
 
 def handle_event(event:, context:)
@@ -22,15 +26,21 @@ def handle_event(event:, context:)
 
   $logger.info('handling event ', event)
 
-  kinesis_client = NYPLRubyUtil::KinesisClient.new(
-    schema_string: ENV['SCHEMA_STRING'],
-    stream_name: ENV['STREAM_NAME']
-  )
-
-  JSON.parse(event["body"]).each do |record|
-    Record.create db_record(record)
-    kinesis_client << record
+  begin
+    records = event["body"].map({|record_string| JSON.parse(record_string)})
+  rescue => e
+    $logger.error('problem parsing JSON for event', e.message)
+    raise e
   end
+
+  begin
+    Record.upsert_all(records, unique_by: :id)
+  rescue => e
+    $logger.error('problem persisting records to database', e.message)
+    raise e
+  end
+
+  records.each {|record| $kinesis_client << record }
 
   respond 200
 end
